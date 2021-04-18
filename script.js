73c5c3a68eb2c87ef8df31f035d80347f8e16170
gabriela-tomazzi <gabtomazzi@gmail.com>
2021-04-18 10:22:00 -0300

    new sections

--- script.js
+++ script.js
@@ -37,3 +37,9 @@ function question4 () {
 	resP.innerHTML = 'resposta da pergunta 4 Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
 	resH2.innerHTML = '';
 }
+
+function copyCel (toCopy) {
+	let text = toCopy;
+	navigator.clipboard.writeText(text);
+	alert(`Copiado`);
+}
